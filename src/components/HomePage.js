d644972e67fea98f3576c04758ec7a6fee633c77
MichaelWhitt <michaelwhitt90@gmail.com>
2021-10-13 16:43:53 -0500

    add homepage styling and link

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,16 @@
 import React, {useState, useEffect} from "react";
-import { Col, Card, Row, CardHeader, CardBody, Jumbotron } from "reactstrap";
+import { Col, Card, Row, CardHeader, CardBody, Jumbotron, Button } from "reactstrap";
 
 export const HomeComponent = () => {
 
     return(
-        <div id="homeSection"></div>
+        <div id="homeSection">
+            <div id="homeText">A Hearing Accessibility App for Hearing-Impaired Movie-Goers</div>
+            <div id="homeText2">Find and rate local theater hearing accomodations</div>
+            <a id="homeLink" href="/search">
+                <Button className="btn-md btn-success" to={"/home"}>Get Started</Button>
+            </a>
+        </div>
         
     )
 }
