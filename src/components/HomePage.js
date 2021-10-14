c9469a61f507bb107b1618297fe17d99fbd20570
MichaelWhitt <michaelwhitt90@gmail.com>
2021-10-13 22:26:26 -0500

    change wording

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,7 @@ export const HomeComponent = () => {
     return(
         <div id="homeSection">
             <div id="homeText">A Hearing Accessibility App for Hearing-Impaired Movie-Goers</div>
-            <div id="homeText2">Find and rate local theater hearing accomodations</div>
+            <div id="homeText2">Find and rate local theater hearing accommodations</div>
             <a id="homeLink" href="/search">
                 <Button className="btn-md btn-success" to={"/home"}>Get Started</Button>
             </a>
