c635a0f514df6fa440dba07d4195162b4c6940fc
MichaelWhitt <michaelwhitt90@gmail.com>
2021-10-13 23:12:25 -0500

    change routing final

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -12,12 +12,12 @@ function Header(props) {
         <Nav className="font-weight-bold headerNav sticky-top">
           <Col>
               <NavItem className="mr-3">
-                <NavLink to={"/home"} style={{fontSize: "1.2rem"}}><FontAwesomeIcon size="lg" icon={faAssistiveListeningSystems}/> Can We Listen? </NavLink>
+                <NavLink to={"/"} style={{fontSize: "1.2rem"}}><FontAwesomeIcon size="lg" icon={faAssistiveListeningSystems}/> Can We Listen? </NavLink>
               </NavItem>
           </Col>
           <Col style={{width: "100%", display: "flex", justifyContent: "right"}}>
               <NavItem className="mr-3">
-                <NavLink to={"/home"}>Home</NavLink>
+                <NavLink to={"/"}>Home</NavLink>
               </NavItem>
               <NavItem className="mr-3 ">
                 <NavLink to={"/search"}>Search</NavLink>
