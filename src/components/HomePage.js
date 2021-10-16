a70e5dab648d5cbe59d39843090522b99be076ba
MichaelWhitt <michaelwhitt90@gmail.com>
2021-10-16 10:55:07 -0500

    fix links

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,9 +9,11 @@ export const HomeComponent = () => {
             <div id="homeText">A Hearing Accessibility App for Hearing-Impaired Movie-Goers</div>
             <div id="homeText2">Find and rate local theater hearing accommodations</div>
             
-                {/* <Button id="homeButton" className="btn-md btn-success" to={"/search"}>Get Started</Button> */}
+               <Button id="homeButton" className="btn-md btn-success" >
+                <Link to={'/search'}>Get Started</Link>
+               </Button>
                 
-                    <Link to={'/search'}>Get Started</Link>
+                    
                 
             
         </div>
