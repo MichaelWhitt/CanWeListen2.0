d0600e3e8eb59a77c4b141b88dc63c08fa7210f2
MichaelWhitt <michaelwhitt90@gmail.com>
2021-10-16 10:44:46 -0500

    add button

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,9 +7,10 @@ export const HomeComponent = () => {
         <div id="homeSection">
             <div id="homeText">A Hearing Accessibility App for Hearing-Impaired Movie-Goers</div>
             <div id="homeText2">Find and rate local theater hearing accommodations</div>
-            <a id="homeLink" href="/search">
-                <Button className="btn-md btn-success" to={"/search"}>Get Started</Button>
-            </a>
+            
+                {/* <Button id="homeButton" className="btn-md btn-success" to={"/search"}>Get Started</Button> */}
+                <a id="homeButton" className="btn-md btn-success" href="/search">Get Started</a>
+            
         </div>
         
     )
